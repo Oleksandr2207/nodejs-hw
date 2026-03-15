1b5411c6582ea66a24e7e4774d07d98b9574e187
Oleksandr2207 <osponomarenko09@gmail.com>
2026-03-15 18:37:15 +0200

    fix bag and install pino

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +10,21 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
